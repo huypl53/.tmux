12906695d7f274cbb12ee7c3c13873569e444df1
huyle <huy@huy.pop>
2021-05-09 21:47:21 +0700

    nvim

--- nvim.old/lua/auto-session/lua.lua
+++
@@ -1,4 +0,0 @@
-require('auto-session').setup {
-    post_restore_cmds = {"tabdo NERDTreeToggle"},
-    pre_save_cmds = {"tabdo NERDTreeClose"}
-}
--- nvim.old/lua/scrollbar/lua.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_command([[
-augroup ScrollbarInit
-  autocmd!
-  autocmd CursorMoved,VimResized,QuitPre * silent! lua require('scrollbar').show()
-  autocmd WinEnter,FocusGained           * silent! lua require('scrollbar').show()
-  autocmd WinLeave,FocusLost             * silent! lua require('scrollbar').clear()
-augroup end
-]])
